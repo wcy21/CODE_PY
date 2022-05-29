8efd145041376df90aa5cc82f1fbb50ced4b4e27
wcy21 <a18325513428@163.com>
2022-05-29 08:12:00 +0800

    2022.5.29

--- 2022.5.28/chapter06/favorite_numbers.py
+++ 2022.5.28/chapter06/favorite_numbers.py
@@ -1,10 +1,13 @@
 favorite_numbers = {
-    'blue_dog': 6,
-    'friendA': 0,
-    'friendB': 5,
-    'friendC': 1,
-    'friendD': 7
+    'blue_dog': [1, 6, 2],
+    'friendA': [0, 9],
+    'friendB': [5, 16, 29],
+    'friendC': [1, 66, 799],
+    'friendD': [7, 8, 12, 18, 25, 67]
 }
 
-for name, number in favorite_numbers.items():
-    print(name + "'s favorite number = " + str(number))
+for name, numbers in favorite_numbers.items():
+    print("\n" + name + "'s favorite numbers:")
+
+    for number in numbers:
+        print("\t" + str(number))
--- 2022.5.28/chapter06/favorite_places_cases.py
+++ 2022.5.28/chapter06/favorite_places_cases.py
@@ -1,3 +1,20 @@
 favorite_places = {
-
+    'alice': {
+        'wonderland'
+    },
+    'blue_dog': {
+        'china',
+        'japan',
+        'london',
+    },
+    'doctor': {
+        'rhodes island',
+        'ursus empire',
+        'kazdel',
+    }
 }
+
+for name, places in favorite_places.items():
+    print("\n" + name.title() + "'s favorite places:")
+    for place in places:
+        print("\t" + place.title())
